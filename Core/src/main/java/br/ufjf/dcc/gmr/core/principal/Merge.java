7f38d6c9abbe47990add1c80cc1e55005845816f
IanCouto <48738352+IanCouto@users.noreply.github.com>
2019-10-21 18:33:15 -0300

    fixing the last commit

--- Core/src/main/java/br/ufjf/dcc/gmr/core/principal/Merge.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/principal/Merge.java
@@ -22,7 +22,7 @@ import java.util.List;
  */
 public class Merge {
     public static void main(String[] args) throws IOException, LocalRepositoryNotAGitRepository, ParseException, OptionNotExist, RepositoryNotFound  {
-        String repository = "C:\\Users\\icout\\OneDrive\\Área de Trabalho\\UFJF";
+        String repository = "";
         List<Formats> list = new ArrayList<>();
         List<Formats> mergeList = new ArrayList<>();
         
