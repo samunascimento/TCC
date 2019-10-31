856efffa57faf0cbeb9a065459a0108b229685c0
antoniopenizollo <antoniopenizollo@gmail.com>
2019-10-31 19:49:35 -0300

    creating Metric class

---
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/Metric.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.core.jasome;
+
+/**
+ *
+ * @author antonio henrique
+ */
+public class Metric {
+    String description;
+    String name;
+    float value; 
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+    
+    
+}
