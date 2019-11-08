ba9faf0da1e94c455c8c90bdee570539a718d12c
Guima01 <54551477+Guima01@users.noreply.github.com>
2019-11-08 01:21:06 -0300

    separating metrics

--- Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/ReadXMLUsingSAX.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/ReadXMLUsingSAX.java
@@ -22,8 +22,12 @@ import org.xml.sax.helpers.DefaultHandler;
 public class ReadXMLUsingSAX extends DefaultHandler {
 
     private String tagAtual;
+    private String project;
+    private String pacckage;
+    private String clazz;
+    private String method;
     private String siglaAtual;
-    
+
     public int numberPackage = 0;
     public List<Metric> listMetric = new ArrayList<>();
 
@@ -56,40 +60,106 @@ public class ReadXMLUsingSAX extends DefaultHandler {
 
     @Override
     public void endDocument() {
-        System.out.println("\nFim do Parsing...");
-        System.out.println(numberPackage);
-        /*
         System.out.println("Imprimindo listMetric");
-        for(int i = 0; i<listMetric.size(); i++){
-            System.out.println(listMetric.get(i).getDescription());
-            System.out.println(listMetric.get(i).getName());
-            System.out.println(listMetric.get(i).getValue());
+        for (int i = 0; i < listMetric.size(); i++) {
+            System.out.print(listMetric.get(i).getDescription() + " ");
+            System.out.print(listMetric.get(i).getName() + " ");
+            System.out.print(listMetric.get(i).getValue() + " ");
+            System.out.println(" ");
+
         }
-        */
+        System.out.println("\nFim do Parsing...");
+
     }
 
+    /**
+     *
+     * @param uri
+     * @param localName
+     * @param qName
+     * @param atts
+     */
     @Override
     public void startElement(String uri, String localName, String qName,
             Attributes atts) {
 
         // recupera o nome da tag atual
         tagAtual = qName;
-        //Contagem MetricPackage,MetricClass,MetricMethod
-        if(tagAtual.equals("Package")){
-            numberPackage++;
+        if (tagAtual.equals("Project")) {
+            project = tagAtual;
+            pacckage = "";
+            clazz = "";
+            method = "";
+        } else if (tagAtual.equals("Package")) {
+            pacckage = tagAtual;
+            project = "";
+            clazz = "";
+            method = "";
+        } else if (tagAtual.equals("Class")) {
+            clazz = tagAtual;
+            project = "";
+            pacckage = "";
+            method = "";
+
+        } else if (tagAtual.equals("Method")) {
+            method = tagAtual;
+            clazz = "";
+            project = "";
+            pacckage = "";
         }
-        //Creating Metric (description,name and value)
-        if (tagAtual.equals("Metric")) {
-            Metric metric = new Metric();
-            
-            String value0 = atts.getValue(0);
-            String value1 = atts.getValue(1);
-            String value2 = atts.getValue(2);
-            
-            metric.setDescription(value0);
-            metric.setName(value1);
-            metric.setValue(value2);
-            listMetric.add(metric);
+        /*Contagem MetricPackage,MetricClass,MetricMethod
+        if (tagAtual.equals("Package")) {
+            numberPackage++;
+        }*/
+        // separando as métricas
+        if (project.equals("Project")) {
+            if (tagAtual.equals("Metric")) {
+                Metric metric = new Metric();
+                String value0 = atts.getValue(0);
+                String value1 = atts.getValue(1);
+                String value2 = atts.getValue(2);
+
+                metric.setDescription(value0);
+                metric.setName(value1);
+                metric.setValue(value2);
+                listMetric.add(metric);
+            }
+        } else if (pacckage.equals("Package")) {
+            if (tagAtual.equals("Metric")) {
+                Metric metric = new Metric();
+                String value0 = atts.getValue(0);
+                String value1 = atts.getValue(1);
+                String value2 = atts.getValue(2);
+
+                metric.setDescription(value0);
+                metric.setName(value1);
+                metric.setValue(value2);
+                listMetric.add(metric);
+            }
+        } else if (clazz.equals("Class")) {
+            if (tagAtual.equals("Metric")) {
+                Metric metric = new Metric();
+                String value0 = atts.getValue(0);
+                String value1 = atts.getValue(1);
+                String value2 = atts.getValue(2);
+
+                metric.setDescription(value0);
+                metric.setName(value1);
+                metric.setValue(value2);
+                listMetric.add(metric);
+            }
+        } else if (method.equals("Method")) {
+            if (tagAtual.equals("Metric")) {
+                Metric metric = new Metric();
+                String value0 = atts.getValue(0);
+                String value1 = atts.getValue(1);
+                String value2 = atts.getValue(2);
+
+                metric.setDescription(value0);
+                metric.setName(value1);
+                metric.setValue(value2);
+                listMetric.add(metric);
+            }
         }
     }
 
@@ -104,7 +174,6 @@ public class ReadXMLUsingSAX extends DefaultHandler {
 
         //vou deixar o método main aqui pra fazer testes com o SAX
         ReadXMLUsingSAX mySax = new ReadXMLUsingSAX();
-        mySax.fazerParsing("C:\\Users\\antonio henrique\\Desktop\\jasome\\build\\distributions\\jasome\\bin\\jasomeXML");
+        mySax.fazerParsing("C:\\Users\\Principal\\Desktop\\UFJF\\sample.xml");
     }
-
 }
