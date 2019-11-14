ccd53a0191d3d7e04de0acca57252427a54185b6
antoniopenizollo <antoniopenizollo@gmail.com>
2019-11-14 13:44:52 -0300

    adding attributes in class MethodMetrics

--- Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/MethodMetrics.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/MethodMetrics.java
@@ -13,14 +13,122 @@ import java.util.List;
  * @author antonio henrique
  */
 public class MethodMetrics {
-    List<Metric> listMetrics = new ArrayList<>();
+    Metric ci;
+    Metric di;
+    Metric fin;
+    Metric fout;
+    Metric iovars;
+    Metric mclc;
+    Metric nbd;
+    Metric ncomp;
+    Metric nop;
+    Metric nvar;
+    Metric si;
+    Metric tloc;
+    Metric vg;
 
-    public List<Metric> getListMetrics() {
-        return listMetrics;
+    public Metric getCi() {
+        return ci;
     }
 
-    public void setListMetrics(List<Metric> listMetrics) {
-        this.listMetrics = listMetrics;
+    public void setCi(Metric ci) {
+        this.ci = ci;
+    }
+
+    public Metric getDi() {
+        return di;
+    }
+
+    public void setDi(Metric di) {
+        this.di = di;
+    }
+
+    public Metric getFin() {
+        return fin;
+    }
+
+    public void setFin(Metric fin) {
+        this.fin = fin;
+    }
+
+    public Metric getFout() {
+        return fout;
+    }
+
+    public void setFout(Metric fout) {
+        this.fout = fout;
+    }
+
+    public Metric getIovars() {
+        return iovars;
+    }
+
+    public void setIovars(Metric iovars) {
+        this.iovars = iovars;
+    }
+
+    public Metric getMclc() {
+        return mclc;
+    }
+
+    public void setMclc(Metric mclc) {
+        this.mclc = mclc;
+    }
+
+    public Metric getNbd() {
+        return nbd;
+    }
+
+    public void setNbd(Metric nbd) {
+        this.nbd = nbd;
+    }
+
+    public Metric getNcomp() {
+        return ncomp;
+    }
+
+    public void setNcomp(Metric ncomp) {
+        this.ncomp = ncomp;
+    }
+
+    public Metric getNop() {
+        return nop;
+    }
+
+    public void setNop(Metric nop) {
+        this.nop = nop;
+    }
+
+    public Metric getNvar() {
+        return nvar;
+    }
+
+    public void setNvar(Metric nvar) {
+        this.nvar = nvar;
+    }
+
+    public Metric getSi() {
+        return si;
+    }
+
+    public void setSi(Metric si) {
+        this.si = si;
+    }
+
+    public Metric getTloc() {
+        return tloc;
+    }
+
+    public void setTloc(Metric tloc) {
+        this.tloc = tloc;
+    }
+
+    public Metric getVg() {
+        return vg;
+    }
+
+    public void setVg(Metric vg) {
+        this.vg = vg;
     }
     
     
