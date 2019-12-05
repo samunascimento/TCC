26e3388ad9d3cc1feed19a823970b1398ea94324
João Pedro Lima <joaop.c.lima12@gmail.com>
2019-12-05 16:48:47 -0300

    Creating SpecialConflictFile

---
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/SpecialConflictFile.java
@@ -0,0 +1,16 @@
+package br.ufjf.dcc.gmr.core.conflictanalysis;
+
+public class SpecialConflictFile {
+	
+	private String fileName;
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	
+}
