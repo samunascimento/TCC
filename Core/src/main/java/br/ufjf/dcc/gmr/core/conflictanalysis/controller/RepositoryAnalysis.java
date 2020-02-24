e88872e9a953bbb22fa85835a7f70a86799a12ae
Luan Ciribelli <luanreisciribelli@ice.ufjf.br>
2020-02-24 11:18:33 -0300

    Updating newlinenumber

--- Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/controller/RepositoryAnalysis.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/controller/RepositoryAnalysis.java
@@ -76,13 +76,35 @@ public interface RepositoryAnalysis {
         //Verificar se a linha existe no arquivo original, e se existir
         List<String> output = new ArrayList<>();
         int counter = 0;
+        int currentLine = 0;
         output = Git.auxiliardiff(directory, commitSource, commitTarget);
         for (String line : output) {
             if (line.charAt(0) == '@' && line.charAt(1) == '@') {
-                String c = line.substring(5);
+                String c = line.substring(5) + line.substring(6);
+                currentLine = Integer.parseInt(c);
             }
+            if (currentLine >= originalLineNumber) {
+                break;
+            } else {
+                if (line.length() > 2 && line.charAt(0) == '+' && line.charAt(1) == '+' && line.charAt(2) == '+') {
+
+                } else if (line.charAt(0) == '+' || line.charAt(1) == '+') {
+                    currentLine++;
+                    counter++;
+                } else if (line.length() > 2 && line.charAt(0) == '-' && line.charAt(1) == '-' && line.charAt(2) == '-') {
+
+                } else if (line.charAt(0) == '-' || line.charAt(1) == '-') {
+                    currentLine++;
+                    counter--;
+                }
+
+            }
+
         }
-        return 0;
+        
+        int newNumber= originalLineNumber - counter;
+
+        return newNumber;
     }
 
 }
