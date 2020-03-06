eade7d68bb624ac3c7e2393323457caf7ca1e5f0
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-03-06 17:01:03 -0300

    view

--- Core/src/main/java/br/ufjf/dcc/gmr/core/JFrame/View.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/JFrame/View.java
@@ -1,6 +1,7 @@
 package br.ufjf.dcc.gmr.core.JFrame;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.io.File;
 import javax.swing.BorderFactory;
@@ -9,6 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.table.DefaultTableModel;
@@ -21,6 +23,7 @@ public class View extends JFrame {
 
     JPanel tableChooserPanel;
     JPanel textPanel;
+    JScrollPane tableChooserScrollPane;
     JTable table;
     JTextArea textArea;
     JFileChooser chooser;
@@ -35,6 +38,7 @@ public class View extends JFrame {
         table = new JTable();
         textArea = new JTextArea();
         progressBar = new JProgressBar();
+        tableChooserScrollPane = new JScrollPane();
     }
 
     private void setTableChooserPanel() {
@@ -60,13 +64,36 @@ public class View extends JFrame {
     }
 
     private void setTable() {
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        table.setShowGrid(true);
-        for (int i = 0, j = 10; i < 10; i++, j--) {
-            Object[] row = {i, j};
+        table.setAutoCreateRowSorter(true);
+        table.setFont(new java.awt.Font("Tahoma", 0, 18));
+        DefaultTableModel model = new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Version", "Id"
+            }
+        ){
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        };
+        for(int i = 100, j = 0;i >= 0;i--,j++){
+            Object[] row = {i,j};
             model.addRow(row);
         }
-
+        table.setModel(model);
     }
 
     private void setMenuBar() {
@@ -82,6 +109,10 @@ public class View extends JFrame {
         progressBar.setPreferredSize(new Dimension(Short.MAX_VALUE, 50));
         progressBar.setBorder(BorderFactory.createTitledBorder("PROGRESS BAR"));
         progressBar.setVisible(false);
+        progressBar.setForeground(Color.BLUE);
+        progressBar.setValue(50);
+        progressBar.setFont(new java.awt.Font("Verdana",0,12) {
+        });
     }
 
     private void showPanel() {
@@ -89,7 +120,8 @@ public class View extends JFrame {
         this.add(textPanel, BorderLayout.CENTER);
         this.add(menuBar, BorderLayout.NORTH);
         this.add(progressBar, BorderLayout.SOUTH);
-        tableChooserPanel.add(table, BorderLayout.CENTER);
+        tableChooserPanel.add(tableChooserScrollPane, BorderLayout.CENTER);
+        tableChooserScrollPane.setViewportView(table);
         textPanel.add(textArea, BorderLayout.CENTER);
         this.setVisible(true);
     }
