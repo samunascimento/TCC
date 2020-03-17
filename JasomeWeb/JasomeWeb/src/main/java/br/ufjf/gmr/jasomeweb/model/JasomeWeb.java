e6999108211732c6893938e767a64048135b6118
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-17 20:06:03 -0300

    return metrics panel

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/gmr/jasomeweb/model/JasomeWeb.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/gmr/jasomeweb/model/JasomeWeb.java
@@ -6,8 +6,19 @@
 package br.ufjf.gmr.jasomeweb.model;
 
 import br.ufjf.dcc.gmr.core.cli.CLIExecution;
+import br.ufjf.dcc.gmr.core.exception.CheckoutError;
+import br.ufjf.dcc.gmr.core.exception.InvalidDocument;
+import br.ufjf.dcc.gmr.core.exception.IsOutsideRepository;
+import br.ufjf.dcc.gmr.core.exception.LocalRepositoryNotAGitRepository;
+import br.ufjf.dcc.gmr.core.exception.RefusingToClean;
+import br.ufjf.dcc.gmr.core.exception.RepositoryNotFound;
+import br.ufjf.dcc.gmr.core.exception.UnknownSwitch;
 import br.ufjf.dcc.gmr.core.jasome.Jasome;
+import br.ufjf.dcc.gmr.core.jasome.JasomeMethods;
+import br.ufjf.dcc.gmr.core.jasome.model.ProjectMetrics;
+import br.ufjf.dcc.gmr.core.vcs.types.Formats;
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,11 +38,14 @@ public class JasomeWeb {
         return listMetrics;
     }
 
-    public List<String> getMetricVersion(String repositoryPath, String jasomePath) throws IOException {
-        Jasome jasome = new Jasome(repositoryPath);
-        CLIExecution extractMetrics = Jasome.extractMetrics(repositoryPath, jasomePath);
+    public List<String> getMetricVersion(String repositoryPath, String jasomePath) throws IOException, RepositoryNotFound, LocalRepositoryNotAGitRepository, ParseException, InvalidDocument, CheckoutError, RefusingToClean, UnknownSwitch, IsOutsideRepository {
+        JasomeMethods jasomeMethods = new JasomeMethods(repositoryPath, jasomePath);
+        //jasomeMethods.runProject(repositoryPath);
+        Formats formats = new Formats(null, "39e7e8da2f0cdc8dfff519d07454e3ddea92e844", null, null);
+        CLIExecution extractMetrics = jasomeMethods.extractMetrics(repositoryPath, jasomePath);
+        ProjectMetrics projectMetrics = jasomeMethods.analyzeVersion(formats, repositoryPath, 0);
+        projectMetrics.getListClassMetrics();
+        return projectMetrics.getListClassMetrics();
         
-        
-        return extractMetrics.getOutput();
     }
 }
