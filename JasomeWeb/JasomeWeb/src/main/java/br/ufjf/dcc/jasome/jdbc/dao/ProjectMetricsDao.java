9f5e4927ffd3fe5cb93b1fcee680d4d473c02401
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-22 16:24:08 -0300

    insert projectmetrics

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/ProjectMetricsDao.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/ProjectMetricsDao.java
@@ -5,10 +5,38 @@
  */
 package br.ufjf.dcc.jasome.jdbc.dao;
 
+import br.ufjf.dcc.gmr.core.db.ConnectionFactory;
+import br.ufjf.dcc.gmr.core.jasome.model.ProjectMetrics;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  *
  * @author anton
  */
 public class ProjectMetricsDao {
+        private final Connection connection;
+
+    public ProjectMetricsDao() {
+        this.connection = ConnectionFactory.getConnection();
+    }
     
+    public void addProjectMetric(ProjectMetrics projectMetrics){
+        String sql = "INSERT INTO tb_projectmetrics " +
+        "(id,sourcedir)" +
+        "VALUES (?,?);";
+
+        try{
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            //set values
+            stmt.setInt(1, projectMetrics.getId());
+            stmt.setString(2, projectMetrics.getSourceDir());
+            stmt.execute();
+            stmt.close();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+        
+    }
 }
