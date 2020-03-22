9dd176ae4426f3788c2571fa3e4db7788bda63ac
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-22 16:24:49 -0300

    adding test class for insertion

---
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/test/TestInsert.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.jasomeweb.test;
+
+import br.ufjf.dcc.gmr.core.jasome.model.ProjectMetrics;
+import br.ufjf.dcc.jasome.jdbc.dao.ProjectMetricsDao;
+
+/**
+ *
+ * @author anton
+ */
+public class TestInsert {
+    public static void main(String[] args) {
+        ProjectMetrics projectMetrics = new ProjectMetrics();
+        projectMetrics.setId(0);
+        projectMetrics.setSourceDir("sourceDir56784");
+        ProjectMetricsDao dao = new ProjectMetricsDao();
+        dao.addProjectMetric(projectMetrics);
+    }
+}
