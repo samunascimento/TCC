278b5242b06fc6e3f1bfa0a22d2777dde5c0114d
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-29 19:28:36 -0300

    adding attributes

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MetricDao.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MetricDao.java
@@ -5,10 +5,114 @@
  */
 package br.ufjf.dcc.jasome.jdbc.dao;
 
+import br.ufjf.dcc.gmr.core.jasome.model.Metric;
+
 /**
  *
  * @author anton
  */
 public class MetricDao {
+    
+    //Metric Package
+    Metric aID;
+    Metric ccrcID;
+    Metric caID;
+    Metric ceID;
+    Metric dmsID;
+    Metric iID;
+    Metric nocID;
+    Metric noiID;
+    Metric pkgRCiID;
+    Metric pkgTCiID;
+    Metric tlocID;
+
+    public Metric getaID() {
+        return aID;
+    }
+
+    public void setaID(Metric aID) {
+        this.aID = aID;
+    }
+
+    public Metric getCcrcID() {
+        return ccrcID;
+    }
+
+    public void setCcrcID(Metric ccrcID) {
+        this.ccrcID = ccrcID;
+    }
+
+    public Metric getCaID() {
+        return caID;
+    }
+
+    public void setCaID(Metric caID) {
+        this.caID = caID;
+    }
+
+    public Metric getCeID() {
+        return ceID;
+    }
+
+    public void setCeID(Metric ceID) {
+        this.ceID = ceID;
+    }
+
+    public Metric getDmsID() {
+        return dmsID;
+    }
+
+    public void setDmsID(Metric dmsID) {
+        this.dmsID = dmsID;
+    }
+
+    public Metric getiID() {
+        return iID;
+    }
+
+    public void setiID(Metric iID) {
+        this.iID = iID;
+    }
+
+    public Metric getNocID() {
+        return nocID;
+    }
+
+    public void setNocID(Metric nocID) {
+        this.nocID = nocID;
+    }
+
+    public Metric getNoiID() {
+        return noiID;
+    }
+
+    public void setNoiID(Metric noiID) {
+        this.noiID = noiID;
+    }
+
+    public Metric getPkgRCiID() {
+        return pkgRCiID;
+    }
+
+    public void setPkgRCiID(Metric pkgRCiID) {
+        this.pkgRCiID = pkgRCiID;
+    }
+
+    public Metric getPkgTCiID() {
+        return pkgTCiID;
+    }
+
+    public void setPkgTCiID(Metric pkgTCiID) {
+        this.pkgTCiID = pkgTCiID;
+    }
+
+    public Metric getTlocID() {
+        return tlocID;
+    }
+
+    public void setTlocID(Metric tlocID) {
+        this.tlocID = tlocID;
+    }
+
     
 }
