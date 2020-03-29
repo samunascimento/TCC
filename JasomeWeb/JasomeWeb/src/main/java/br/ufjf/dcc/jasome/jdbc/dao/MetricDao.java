05579b1fe5c7f4f1f62c87106d0a6d31794e0aaa
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-29 19:37:12 -0300

    adding attributes (MetricDAO <- classMetrics)

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MetricDao.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MetricDao.java
@@ -13,7 +13,7 @@ import br.ufjf.dcc.gmr.core.jasome.model.Metric;
  */
 public class MetricDao {
     
-    //Metric Package
+    //Package Metric
     Metric aID;
     Metric ccrcID;
     Metric caID;
@@ -25,7 +25,50 @@ public class MetricDao {
     Metric pkgRCiID;
     Metric pkgTCiID;
     Metric tlocID;
-
+    
+    //Class Metric
+    Metric aaID;
+    Metric adID;
+    Metric aiID;
+    Metric aitID;
+    Metric aoID;
+    Metric avID;
+    Metric clrciID;
+    Metric cltciID;
+    Metric ditID;
+    Metric hmdID;
+    Metric hmiID;
+    Metric mhfID;
+    Metric mifID;
+    Metric maID;
+    Metric mdID;
+    Metric miID;
+    Metric mitID;
+    Metric moID;
+    Metric nfID;
+    Metric nmID;
+    Metric nmaID;
+    Metric nmiID;
+    Metric noaID;
+    Metric nochID;
+    Metric nodID;
+    Metric nolID;
+    Metric nopaID;
+    Metric normID;
+    Metric npfID;
+    Metric npmID;
+    Metric nsfID;
+    Metric nsmID;
+    Metric pmrID;
+    Metric pmdID;
+    Metric pmiID;
+    Metric rtlocID;
+    Metric sixID;
+    //Metric tlocID;
+    Metric wmcID;
+    
+    
+    //Package Metric
     public Metric getaID() {
         return aID;
     }
@@ -106,6 +149,7 @@ public class MetricDao {
         this.pkgTCiID = pkgTCiID;
     }
 
+    //  * Atributo comum para ClassMetric e MethodMetric
     public Metric getTlocID() {
         return tlocID;
     }
@@ -114,5 +158,314 @@ public class MetricDao {
         this.tlocID = tlocID;
     }
 
+    //Class Metrics
+
+    public Metric getAaID() {
+        return aaID;
+    }
+
+    public void setAaID(Metric aaID) {
+        this.aaID = aaID;
+    }
+
+    public Metric getAdID() {
+        return adID;
+    }
+
+    public void setAdID(Metric adID) {
+        this.adID = adID;
+    }
+
+    public Metric getAiID() {
+        return aiID;
+    }
+
+    public void setAiID(Metric aiID) {
+        this.aiID = aiID;
+    }
+
+    public Metric getAitID() {
+        return aitID;
+    }
+
+    public void setAitID(Metric aitID) {
+        this.aitID = aitID;
+    }
+
+    public Metric getAoID() {
+        return aoID;
+    }
+
+    public void setAoID(Metric aoID) {
+        this.aoID = aoID;
+    }
+
+    public Metric getAvID() {
+        return avID;
+    }
+
+    public void setAvID(Metric avID) {
+        this.avID = avID;
+    }
+
+    public Metric getClrciID() {
+        return clrciID;
+    }
+
+    public void setClrciID(Metric clrciID) {
+        this.clrciID = clrciID;
+    }
+
+    public Metric getCltciID() {
+        return cltciID;
+    }
+
+    public void setCltciID(Metric cltciID) {
+        this.cltciID = cltciID;
+    }
+
+    public Metric getDitID() {
+        return ditID;
+    }
+
+    public void setDitID(Metric ditID) {
+        this.ditID = ditID;
+    }
+
+    public Metric getHmdID() {
+        return hmdID;
+    }
+
+    public void setHmdID(Metric hmdID) {
+        this.hmdID = hmdID;
+    }
+
+    public Metric getHmiID() {
+        return hmiID;
+    }
+
+    public void setHmiID(Metric hmiID) {
+        this.hmiID = hmiID;
+    }
+
+    public Metric getMhfID() {
+        return mhfID;
+    }
+
+    public void setMhfID(Metric mhfID) {
+        this.mhfID = mhfID;
+    }
+
+    public Metric getMifID() {
+        return mifID;
+    }
+
+    public void setMifID(Metric mifID) {
+        this.mifID = mifID;
+    }
+
+    public Metric getMaID() {
+        return maID;
+    }
+
+    public void setMaID(Metric maID) {
+        this.maID = maID;
+    }
+
+    public Metric getMdID() {
+        return mdID;
+    }
+
+    public void setMdID(Metric mdID) {
+        this.mdID = mdID;
+    }
+
+    public Metric getMiID() {
+        return miID;
+    }
+
+    public void setMiID(Metric miID) {
+        this.miID = miID;
+    }
+
+    public Metric getMitID() {
+        return mitID;
+    }
+
+    public void setMitID(Metric mitID) {
+        this.mitID = mitID;
+    }
+
+    public Metric getMoID() {
+        return moID;
+    }
+
+    public void setMoID(Metric moID) {
+        this.moID = moID;
+    }
+
+    public Metric getNfID() {
+        return nfID;
+    }
+
+    public void setNfID(Metric nfID) {
+        this.nfID = nfID;
+    }
+
+    public Metric getNmID() {
+        return nmID;
+    }
+
+    public void setNmID(Metric nmID) {
+        this.nmID = nmID;
+    }
+
+    public Metric getNmaID() {
+        return nmaID;
+    }
+
+    public void setNmaID(Metric nmaID) {
+        this.nmaID = nmaID;
+    }
+
+    public Metric getNmiID() {
+        return nmiID;
+    }
+
+    public void setNmiID(Metric nmiID) {
+        this.nmiID = nmiID;
+    }
+
+    public Metric getNoaID() {
+        return noaID;
+    }
+
+    public void setNoaID(Metric noaID) {
+        this.noaID = noaID;
+    }
+
+    public Metric getNochID() {
+        return nochID;
+    }
+
+    public void setNochID(Metric nochID) {
+        this.nochID = nochID;
+    }
+
+    public Metric getNodID() {
+        return nodID;
+    }
+
+    public void setNodID(Metric nodID) {
+        this.nodID = nodID;
+    }
+
+    public Metric getNolID() {
+        return nolID;
+    }
+
+    public void setNolID(Metric nolID) {
+        this.nolID = nolID;
+    }
+
+    public Metric getNopaID() {
+        return nopaID;
+    }
+
+    public void setNopaID(Metric nopaID) {
+        this.nopaID = nopaID;
+    }
+
+    public Metric getNormID() {
+        return normID;
+    }
+
+    public void setNormID(Metric normID) {
+        this.normID = normID;
+    }
+
+    public Metric getNpfID() {
+        return npfID;
+    }
+
+    public void setNpfID(Metric npfID) {
+        this.npfID = npfID;
+    }
+
+    public Metric getNpmID() {
+        return npmID;
+    }
+
+    public void setNpmID(Metric npmID) {
+        this.npmID = npmID;
+    }
+
+    public Metric getNsfID() {
+        return nsfID;
+    }
+
+    public void setNsfID(Metric nsfID) {
+        this.nsfID = nsfID;
+    }
+
+    public Metric getNsmID() {
+        return nsmID;
+    }
+
+    public void setNsmID(Metric nsmID) {
+        this.nsmID = nsmID;
+    }
+
+    public Metric getPmrID() {
+        return pmrID;
+    }
+
+    public void setPmrID(Metric pmrID) {
+        this.pmrID = pmrID;
+    }
+
+    public Metric getPmdID() {
+        return pmdID;
+    }
+
+    public void setPmdID(Metric pmdID) {
+        this.pmdID = pmdID;
+    }
+
+    public Metric getPmiID() {
+        return pmiID;
+    }
+
+    public void setPmiID(Metric pmiID) {
+        this.pmiID = pmiID;
+    }
+
+    public Metric getRtlocID() {
+        return rtlocID;
+    }
+
+    public void setRtlocID(Metric rtlocID) {
+        this.rtlocID = rtlocID;
+    }
+
+    public Metric getSixID() {
+        return sixID;
+    }
+
+    public void setSixID(Metric sixID) {
+        this.sixID = sixID;
+    }
+
+    public Metric getWmcID() {
+        return wmcID;
+    }
+
+    public void setWmcID(Metric wmcID) {
+        this.wmcID = wmcID;
+    }
+    
+    // Method Metric
+    
+    
     
 }
