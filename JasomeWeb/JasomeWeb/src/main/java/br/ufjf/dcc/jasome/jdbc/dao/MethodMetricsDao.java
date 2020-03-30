d054252f970c041483677e50f0c193524bef5518
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-30 19:57:25 -0300

    Adding delete method to the MethodMetricsDao class

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MethodMetricsDao.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/jasome/jdbc/dao/MethodMetricsDao.java
@@ -5,10 +5,40 @@
  */
 package br.ufjf.dcc.jasome.jdbc.dao;
 
+import br.ufjf.dcc.gmr.core.jasome.model.MethodMetrics;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author anton
  */
 public class MethodMetricsDao {
+    private Connection connection;
+    
+    public int insert(MethodMetrics methodMetrics){
+        return 0;
+        
+    }
+    
+    public List<MethodMetrics> select(){
+        return null;
+    }
+    
+    public void delete(int id){
+        String sql = "DELETE FROM tb_methodMetrics WHERE id = ? ";
+        
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            //set values
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     
 }
