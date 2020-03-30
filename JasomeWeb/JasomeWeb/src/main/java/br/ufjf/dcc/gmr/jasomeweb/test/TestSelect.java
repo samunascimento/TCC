a646e24d65b4c66650d8336b6ee7a3e3dcfa627b
antoniopenizollo <antoniopenizollo@gmail.com>
2020-03-30 19:46:13 -0300

    adding test classes

---
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/test/TestSelect.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.jasomeweb.test;
+
+import br.ufjf.dcc.gmr.core.jasome.model.ProjectMetrics;
+import br.ufjf.dcc.jasome.jdbc.dao.ProjectMetricsDao;
+
+/**
+ *
+ * @author anton
+ */
+public class TestSelect {
+    public static void main(String[] args) {
+        
+    }
+}
