bae9731436faa69a6eb150b31a0a8c1dbd8a50b6
Principal <Principal@DESKTOP-2TESFLC>
2020-04-07 20:30:05 -0300

    adding atributtes to ProjectMetrics

--- Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/model/ProjectMetrics.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/jasome/model/ProjectMetrics.java
@@ -9,10 +9,17 @@ import java.util.List;
  */
 public class ProjectMetrics {
 
+
     private List<VersionMetrics> listVersionMetrics = new ArrayList<>();
 
     private String sourceDir;
     private int id;
+    private String url;
+    private String organization;
+    private String name;
+    
+    
+    
     private List<String> listPackageMetrics = new ArrayList<>();
     private List<String> listClassMetrics  = new ArrayList<>();
     private List<String> listMethodMetrics  = new ArrayList<>();
@@ -135,6 +142,48 @@ public class ProjectMetrics {
 
     public void setListMethodMetrics(List<String> listMethodMetrics) {
         this.listMethodMetrics = listMethodMetrics;
+    }
+    
+      /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the organization
+     */
+    public String getOrganization() {
+        return organization;
+    }
+
+    /**
+     * @param organization the organization to set
+     */
+    public void setOrganization(String organization) {
+        this.organization = organization;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
     }
     
 }
