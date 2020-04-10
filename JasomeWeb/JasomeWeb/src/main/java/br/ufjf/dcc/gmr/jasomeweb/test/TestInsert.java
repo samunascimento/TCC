ac8c192efba2fcb5d1bad8419e1e4a51b29f654c
antoniopenizollo <antoniopenizollo@gmail.com>
2020-04-10 18:20:10 -0300

    Adding tests

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/test/TestInsert.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/test/TestInsert.java
@@ -5,11 +5,15 @@
  */
 package br.ufjf.dcc.gmr.jasomeweb.test;
 
+import br.ufjf.dcc.gmr.core.jasome.model.ClassMetrics;
 import br.ufjf.dcc.gmr.core.jasome.model.Metric;
 import br.ufjf.dcc.gmr.core.jasome.model.VersionMetrics;
+import br.ufjf.dcc.jasome.jdbc.dao.ClassMetricsDao;
 import br.ufjf.dcc.jasome.jdbc.dao.MetricDao;
 import br.ufjf.dcc.jasome.jdbc.dao.VersionMetricsDao;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -19,26 +23,89 @@ public class TestInsert {
 
     public static void main(String[] args) throws SQLException {
 
-        Metric a = new Metric();
-        a.setName("metric geral");
-        a.setDescription("teste descricao");
-        a.setValue(10.00);
-        a.setId(2);
+//        Metric a = new Metric();
+//        a.setName("metric geral 112");
+//        a.setDescription("teste descricao 112");
+//        a.setValue(1110.00);
+//        a.setId(100);
+//
+//        VersionMetricsDao versionMetricsDao = new VersionMetricsDao();
+//        VersionMetrics versionMetrics = new VersionMetrics();
+//
+//        MetricDao dao = new MetricDao();
+//        int insert = dao.insert(a);
+//        a.setId(insert);
+//
+//        versionMetrics.setTloc(a);
+//
+//        int insertVersion = versionMetricsDao.insert(versionMetrics);
+//
+//        System.out.println("insert = " + insert);
+//        System.out.println("insertVersion = " + insertVersion);
 
-        VersionMetricsDao versionMetricsDao = new VersionMetricsDao();
-        VersionMetrics versionMetrics = new VersionMetrics();
+        //teste de classMetricsDao
+        ClassMetricsDao classMetricsDao = new ClassMetricsDao();
+        ClassMetrics classMetrics = new ClassMetrics();
+
+        Metric a1 = new Metric();
+        a1.setName("name metric class");
+        a1.setDescription("metrica da classe");
+        a1.setValue(404);
+        a1.setId(10);
         
-
-        MetricDao dao = new MetricDao();
-        int insert = dao.insert(a);
-        a.setId(insert);
-
-        versionMetrics.setTloc(a);
-
-        int insertVersion = versionMetricsDao.insert(versionMetrics);
-
-        System.out.println("insert = " + insert);
-        System.out.println("insertVersion = " + insertVersion);
+        
+        for(int i=1;i<=39;i++){
+            MetricDao metricDao = new MetricDao();
+            int insertMetric = metricDao.insert(a1);
+            System.out.println(insertMetric);
+            
+            a1.setId(insertMetric);
+            classMetrics.setAa(a1);
+            classMetrics.setAd(a1);
+            classMetrics.setAi(a1);
+            classMetrics.setAit(a1);
+            classMetrics.setAo(a1);
+            classMetrics.setAv(a1);
+            classMetrics.setClrci(a1);
+            classMetrics.setCltci(a1);
+            classMetrics.setDit(a1);
+            classMetrics.setHmd(a1);
+            classMetrics.setHmi(a1);
+            classMetrics.setMhf(a1);
+            classMetrics.setMif(a1);
+            classMetrics.setMa(a1);
+            classMetrics.setMd(a1);
+            classMetrics.setMi(a1);
+            classMetrics.setMit(a1);
+            classMetrics.setMo(a1);
+            classMetrics.setNf(a1);
+            classMetrics.setNm(a1);
+            classMetrics.setNma(a1);
+            classMetrics.setNmi(a1);
+            classMetrics.setNoa(a1);
+            classMetrics.setNoch(a1);
+            classMetrics.setNod(a1);
+            classMetrics.setNol(a1);
+            classMetrics.setNopa(a1);
+            classMetrics.setNorm(a1);
+            classMetrics.setNpf(a1);
+            classMetrics.setNpm(a1);
+            classMetrics.setNsf(a1);
+            classMetrics.setNsm(a1);
+            classMetrics.setPmr(a1);
+            classMetrics.setPmd(a1);
+            classMetrics.setPmi(a1);
+            classMetrics.setRtloc(a1);
+            classMetrics.setSix(a1);
+            classMetrics.setTloc(a1);
+            classMetrics.setWmc(a1);
+        }
+        
+        classMetricsDao.insert(classMetrics);
+        
+        List<ClassMetrics> listClassMetrics = new ArrayList<>();
+            
+        listClassMetrics = classMetricsDao.select();
 
     }
 }
