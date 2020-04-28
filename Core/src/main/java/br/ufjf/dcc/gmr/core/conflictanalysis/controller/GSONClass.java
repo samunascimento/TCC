c428ee77033a13651c1dbda8504d5a69b2e2f252
LuanCiribelli <luanreisciribelli@gmail.com>
2020-04-28 18:01:11 -0300

    Gson Class

--- Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/controller/GSONClass.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/controller/GSONClass.java
@@ -1,9 +1,17 @@
 package br.ufjf.dcc.gmr.core.conflictanalysis.controller;
 
+import br.ufjf.dcc.gmr.core.conflictanalysis.model.MergeEvent;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.Collection;
+import java.util.List;
 
 /**
  *
@@ -11,15 +19,29 @@ import java.io.OutputStreamWriter;
  */
 public class GSONClass {
 
-    public static void createGson(String path, String save, String saveFileName) throws IOException {
+    public static void save(String path, List<MergeEvent> event) throws IOException {
 
-        File myFile = new File(path + "/" + saveFileName);
+        Gson toSave = new Gson();
+        String toJson = toSave.toJson(event);
+        File myFile = new File(path);
         myFile.createNewFile();
         FileOutputStream fOut = new FileOutputStream(myFile);
         OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
-        myOutWriter.append(save);
+        myOutWriter.append(toJson);
         myOutWriter.close();
         fOut.close();
 
     }
+
+    public static List<MergeEvent> read(String path) throws FileNotFoundException {
+
+        Gson recover = new Gson();
+        List<MergeEvent> get = null;
+        FileReader read = new FileReader(path);   
+        Type myType = new TypeToken<List<MergeEvent>>(){}.getType();
+        List<MergeEvent> mergeEvents = recover.fromJson(read, myType);
+        
+        return mergeEvents;
+    }
+
 }
