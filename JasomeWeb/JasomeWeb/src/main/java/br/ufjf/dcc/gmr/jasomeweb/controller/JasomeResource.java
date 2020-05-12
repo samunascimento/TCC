1c9a598cbacacc3392562ef4807f02d1458a0e4d
antoniopenizollo <antoniopenizollo@gmail.com>
2020-05-11 21:03:47 -0300

    adding method to return package metrics

--- JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/controller/JasomeResource.java
+++ JasomeWeb/JasomeWeb/src/main/java/br/ufjf/dcc/gmr/jasomeweb/controller/JasomeResource.java
@@ -18,6 +18,7 @@ import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.core.MediaType;
@@ -68,10 +69,17 @@ public class JasomeResource {
     }
     
     @GET
-    @Path("versionPackage")
+    @Path("metrics/package")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getVersionPackage(){
-        return null;
+    public String getMetricsPackage() throws SQLException{
+        Connection connection = ConnectionFactory.getConnection();
+        MetricDao dao = new MetricDao(connection);
+        Gson g = new Gson();
+        List<Metric> list = new ArrayList<>();
+        list = dao.selectMetricPackage();
+        String listJ = g.toJson(list);
+        System.out.println("entrou aqui");
+        return listJ;
     }
 
     /**
