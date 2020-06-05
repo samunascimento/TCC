10401779bfe3a5ec6a3781369a2da37e64b36ad6
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-06-05 11:28:33 -0300

    Creating class Person

--- Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/analysis/example/Person.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/analysis/example/Person.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufjf.dcc.gmr.core.chunks.antlr4.analysis.example;
 
-/**
- *
- * @author icout
- */
 public class Person {
-    
+
+    private String name;
+    private String CPF;
+    private String Adress;
+    private String Age;
+
+    public Person(String name, String CPF, String Adress, String Age) {
+        this.name = name;
+        this.CPF = CPF;
+        this.Adress = Adress;
+        this.Age = Age;
+    }
+
+    public Person() {
+    }
+
+    /**
+     * @return the Adress
+     */
+    public String getAdress() {
+        return Adress;
+    }
+
+    /**
+     * @return the Age
+     */
+    public String getAge() {
+        return Age;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param Adress the Adress to set
+     */
+    public void setAdress(String Adress) {
+        this.Adress = Adress;
+    }
+
+    /**
+     * @param Age the Age to set
+     */
+    public void setAge(String Age) {
+        this.Age = Age;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
