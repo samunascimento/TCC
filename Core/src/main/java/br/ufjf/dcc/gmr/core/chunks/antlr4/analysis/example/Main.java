e7d3fb6c670c82ebb8e3cc39e4a4a68a50ca5c3a
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-06-05 11:25:47 -0300

    adding antlr4.analysis.example

---
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/analysis/example/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.core.chunks.antlr4.analysis.example;
+
+/**
+ *
+ * @author icout
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
