3c9fdc6afa3b11bb11540d92b67b893ffac2cb3a
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-06-21 10:29:57 -0300

    update analysis.example

--- Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/analysis/example/Employee.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/analysis/example/Employee.java
@@ -9,20 +9,16 @@ package br.ufjf.dcc.gmr.core.chunks.antlr4.analysis.example;
  *
  * @author icout
  */
-public class Employee extends Person {
+public class Employee {
 
     private String office;
     private String registration;
+    private String name;
+    private String CPF;
+    private String adress;
     private float salary;
     private boolean active;
-
-    public Employee(String office, String registration, float salary, boolean active, String name, String CPF, String Adress, int Age) {
-        super(name, CPF, Adress, Age);
-        this.office = office;
-        this.registration = registration;
-        this.salary = salary;
-        this.active = active;
-    }
+    private int age;
 
     public String getOffice() {
         return office;
@@ -62,5 +58,79 @@ public class Employee extends Person {
 
     public void dismiss() {
         this.active = false;
+    }
+
+    public void setInfo(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setInfo(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    /**
+     * @return the adress
+     */
+    public String getAdress() {
+        return adress;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param Adress the adress to set
+     */
+    public void setAdress(String Adress) {
+        this.adress = Adress;
+    }
+
+    /**
+     * @param Age the age to set
+     */
+    public void setAge(int Age) {
+        this.age = Age;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @param name1 the name to set
+     * @param name2
+     */
+    public void setName(String name1, String name2) {
+        this.name = name1;
     }
 }
