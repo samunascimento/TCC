7f19cd794a5cf79168d1ce9bc14632b7390aeee1
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-07-12 19:58:57 -0300

    fixing equalsTo MethodDeclarationBinding

--- Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/visitor/MyVisitor.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/visitor/MyVisitor.java
@@ -797,7 +797,7 @@ public class MyVisitor extends JavaParserBaseVisitor<Object> {
 
     @Override
     public Object visitClassBodyDeclaration(JavaParser.ClassBodyDeclarationContext ctx) {
-        if (ctx.getChild(1).getText().equals("static")) {
+        if (ctx.getChild(1)!= null && ctx.getChild(1).getText().equals("static")) {
             this.isStatic = true;
         } else {
             this.isStatic = false;
