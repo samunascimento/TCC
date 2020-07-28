c0af06177b833ba26172e09c2956d2cba95137b4
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-07-28 10:27:18 -0300

    fixing bugs

--- Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/binding/PrimitiveTypes.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/binding/PrimitiveTypes.java
@@ -12,31 +12,25 @@ public class PrimitiveTypes {
 
     public static boolean isCompatibleType(String subType, String type) {
 
-        try {
-            if ((subType != null && type == null) || (subType == null && type != null)) {
-                return false;
-            } else if (subType == null && type == null) {
-                return true;
-            } else if (subType.equals(INT) && (type.equals(INT) || type.equals(FLOAT) || type.equals(DOUBLE))) {
-                return true;
-            } else if (subType.equals(FLOAT) && (type.equals(FLOAT) || type.equals(DOUBLE))) {
-                return true;
-            } else if (subType.equals(DOUBLE) && type.equals(DOUBLE)) {
-                return true;
-            } else if (subType.equals(BOOLEAN) && type.equals(BOOLEAN)) {
-                return true;
-            } else if (subType.equals(CHAR) && type.equals(CHAR)) {
-                return true;
-            } else if (subType.equals(STRING) && type.equals(STRING)) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            System.out.println("@@@@@@@@@@@@@@@@@@@errorLine33@@@@@@@@@@@@@@@@@@@");
-            e.printStackTrace();
-
+        if ((subType != null && type == null) || (subType == null && type != null)) {
+            return false;
+        } else if (subType == null && type == null) {
+            return true;
+        } else if (subType.equals(INT) && (type.equals(INT) || type.equals(FLOAT) || type.equals(DOUBLE))) {
+            return true;
+        } else if (subType.equals(FLOAT) && (type.equals(FLOAT) || type.equals(DOUBLE))) {
+            return true;
+        } else if (subType.equals(DOUBLE) && type.equals(DOUBLE)) {
+            return true;
+        } else if (subType.equals(BOOLEAN) && type.equals(BOOLEAN)) {
+            return true;
+        } else if (subType.equals(CHAR) && type.equals(CHAR)) {
+            return true;
+        } else if (subType.equals(STRING) && type.equals(STRING)) {
+            return true;
+        } else {
+            return false;
         }
-        return false;
+
     }
 }
