4da367be5803bcee79850511127a8abe442caea5
Luan Ciribelli <luanreisciribelli@gmail.com>
2020-09-20 16:26:14 -0300

    database classes

---
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/conflictanalysis/dao/SyntaxStructureConflictRegionSyntaxV1DAO.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.core.conflictanalysis.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author luan
+ */
+public class SyntaxStructureConflictRegionSyntaxV1DAO {
+
+    private Connection connection;
+
+    public static final String SYNTAX_STRUCTURE_ID = "idsyntaxStructure";
+    public static final String CONFLICT_REGION_ID = "idConflictRegion";
+
+    public SyntaxStructureConflictRegionSyntaxV1DAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public boolean insert(int syntaxStructureID, int conflcitRegionID) throws SQLException {
+
+        String sql = "INSERT INTO ConflictFile_MergeEvent_SyntaxV1 "
+                + "(" + SYNTAX_STRUCTURE_ID + ", " + CONFLICT_REGION_ID + ") "
+                + "VALUES (?,?)";
+
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = connection.prepareStatement(sql);
+
+            stmt.setInt(1, syntaxStructureID);
+            stmt.setInt(2, conflcitRegionID);
+
+            stmt.executeUpdate();
+
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (stmt != null) {
+                stmt.close();
+            }
+        }
+    }
+}
