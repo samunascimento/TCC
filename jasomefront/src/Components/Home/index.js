db28390ece2056adf8f58a3ac2d28915b3c19564
beatrizcunharo <beatrizcunha@ice.ufjf.br>
2020-11-20 19:41:43 -0300

    Ajustando front de cadastro de projeto

--- jasomefront/src/Components/Home/index.js
+++ jasomefront/src/Components/Home/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default class Home extends Component{
 
@@ -23,9 +24,15 @@ export default class Home extends Component{
                     type="text"
                     placeholder="Projeto"
                     style={{
+                        width: '50%',
                         padding: '0px 0px 0px 10px'
                     }}
                 />
+            </div>
+            <div style={{ margin: '30px 0px 0px 16px',}}>
+                <Button color='primary' variant='contained' size='medium'>
+                    Cadastrar
+                </Button>
             </div>
             </>
         );
