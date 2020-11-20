731163119ecd55a876e38f1cd21b2331dbadadd6
beatrizcunharo <beatrizcunha@ice.ufjf.br>
2020-11-20 01:04:26 -0300

    Iniciando front de cadastro de projeto

---
+++ jasomefront/src/Components/Home/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+export default class Home extends Component{
+
+    render(){
+        return(
+            <>
+            <div>
+                <Typography variant='h4' style={{
+                    padding: '16px 0px 0px 16px'
+                }}>
+                    Cadastro de Projeto
+                </Typography>
+            </div>
+            <div style={{display:'flex', padding: '100px 0px 0px 16px'}}>
+                <Typography variant='h6'>
+                    Caminho para o projeto:
+                </Typography>
+                <TextField 
+                    id="project" 
+                    type="text"
+                    placeholder="Projeto"
+                    style={{
+                        padding: '0px 0px 0px 10px'
+                    }}
+                />
+            </div>
+            </>
+        );
+    }
+
+}
