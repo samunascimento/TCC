0a849eaeda9156a0768174c0e135be9e828863e5
Luan Ciribelli <luanreisciribelli@gmail.com>
2021-01-05 15:39:23 -0300

    setting python tests

--- Core/src/main/java/br/ufjf/dcc/gmr/core/vcs/test/TestLuan.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/vcs/test/TestLuan.java
@@ -3,6 +3,7 @@ package br.ufjf.dcc.gmr.core.vcs.test;
 import br.ufjf.dcc.gmr.core.conflictanalysis.controller.ConflictAnalysisTools;
 import br.ufjf.dcc.gmr.core.mergenature.antlr4.ANTLR4Results;
 import br.ufjf.dcc.gmr.core.mergenature.antlr4.ANTLR4Tools;
+import br.ufjf.dcc.gmr.core.mergenature.antlr4.SyntaxStructure;
 import java.io.IOException;
 
 /**
@@ -14,7 +15,10 @@ public class TestLuan {
     public static void main(String[] args) throws IOException {
 
          ANTLR4Results teste = ConflictAnalysisTools.getANTLR4ResultsInInterval("/home/luan/Github/teste.py", 1, 34);
-         
+         for (SyntaxStructure outmostedCommentAnalysi : teste.getOutmostedCommentAnalysis()) {
+             System.out.println(outmostedCommentAnalysi.toString());
+            
+        }
     }
 
 }
