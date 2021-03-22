ff77f49e6e8f3bdef0539405ec72ced749f12b33
IanCouto <48738352+IanCouto@users.noreply.github.com>
2021-03-22 17:32:22 -0300

    creating a model for Dependency

---
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/model/Dependency.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.ufjf.dcc.gmr.core.chunks.antlr4.model;
+
+import br.ufjf.dcc.gmr.core.chunks.antlr4.binding.DependencyType;
+import br.ufjf.dcc.gmr.core.vcs.types.ConflictChunk;
+
+public class Dependency {
+    private ConflictChunk chunk1;
+    private ConflictChunk chunk2;
+    private String label;
+    private DependencyType type;
+
+    public Dependency(ConflictChunk chunk1, ConflictChunk chunk2, String label, DependencyType type) {
+        this.chunk1 = chunk1;
+        this.chunk2 = chunk2;
+        this.label = label;
+        this.type = type;
+    }
+
+    public DependencyType getType() {
+        return type;
+    }
+
+    public void setType(DependencyType type) {
+        this.type = type;
+    }
+
+    public ConflictChunk getChunk1() {
+        return chunk1;
+    }
+
+    public void setChunk1(ConflictChunk chunk1) {
+        this.chunk1 = chunk1;
+    }
+
+    public ConflictChunk getChunk2() {
+        return chunk2;
+    }
+
+    public void setChunk2(ConflictChunk chunk2) {
+        this.chunk2 = chunk2;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+    
+}
