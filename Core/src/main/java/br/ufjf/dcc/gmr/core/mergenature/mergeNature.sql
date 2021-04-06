39f9ed34b2f19b413958b23b75bebeb6fa55d225
João Pedro Lima <joaop.c.lima12@gmail.com>
2021-04-06 20:09:51 -0300

    Fixing mergeNature.sql

--- Core/src/main/java/br/ufjf/dcc/gmr/core/mergenature/mergeNature.sql
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/mergenature/mergeNature.sql
@@ -1,40 +1,42 @@
-CREATE TABLE Project(
-   	id SERIAL PRIMARY KEY ,
+create table Project(
+   	id SERIAL PRIMARY KEY (ID),
     name VARCHAR(100),
     url VARCHAR(300),
-    organization VARCHAR(100)
+    organization VARCHAR(100),
+    PRIMARY KEY (ID)
 );
 
-CREATE TABLE Merge(
-	id SERIAL PRIMARY KEY ,
-	fk_ancestor INT, FOREIGN KEY (fk_ancestor) REFERENCES Commit(id),
-	fk_merge INT NOT NULL,FOREIGN KEY (idCommit) REFERENCES Commit(id)) ,
-	mergeType INT
+create table Merge(
+	id SERIAL PRIMARY KEY (ID),
+	fk_ancestor FOREIGN KEY,
+	fk_merge FOREIGN KEY NOT NULL,
+	mergeType INT,
 );
 
 CREATE TABLE Commit(
-	id SERIAL PRIMARY KEY ,
+	id SERIAL PRIMARY KEY (ID),
 	message VARCHAR(100),
 	commitHash VARCHAR(20),
 	author VARCHAR(100),
  	authorDate DATE,
  	committer VARCHAR(100),
- 	committerDate DATE
+ 	committerDate DATE,
+	PRIMARY KEY (ID)
 );
 
 CREATE TABLE Conflict(
-	id SERIAL PRIMARY KEY ,
+	id SERIAL PRIMARY KEY (ID),
 	parent1FilePath VARCHAR(500),
 	parent2FilePath VARCHAR(500),
 	ancestorFilePath VARCHAR(500),
 	hasOutsideAlterations BOOLEAN,
 	hasOutsideAlterationsIgnoringFormatting BOOLEAN,
-	conflictType INT
-	
+	conflictType INT,
+	PRIMARY KEY (ID)
 );
 
 CREATE TABLE ConflictRegion(
-	id SERIAL PRIMARY KEY,
+	id SERIAL PRIMARY KEY (ID),
 	rawConflict TEXT,
 	beforeContext TEXT,
 	beginLine INT,
@@ -47,29 +49,29 @@ CREATE TABLE ConflictRegion(
 	outmostedStructures TEXT,
 	originalV1FirstLine INT,
 	originalV2FirstLine INT,
-	developerDecision INT
+	developerDecision INT,
 );
 
 CREATE TABLE Project_Merge(
-	idProject INT , FOREIGN KEY(idProject) REFERENCES Project(id),
-	idMerge INT , FOREIGN KEY (idMerge) REFERENCES Merge(id),
+	idProject INT PRIMARY KEY, FOREIGN KEY(idProject) REFERENCES Project(id),
+	idMerge INT PRIMARY KEY, FOREIGN KEY (idMerge) REFERENCES Merge(id),
 	CONSTRAINT project_merge_Key PRIMARY KEY (idProject, idMerge)
 );
 
 CREATE TABLE Merge_Commit_parents(
-	idMerge INT , FOREIGN KEY(idMerge) REFERENCES Merge(id),
-	idCommit INT , FOREIGN KEY (idCommit) REFERENCES Commit(id),
+	idMerge INT PRIMARY KEY, FOREIGN KEY(idMerge) REFERENCES Merge(id),
+	idCommit INT PRIMARY KEY, FOREIGN KEY (idCommit) REFERENCES Commit(id),
 	CONSTRAINT merge_commit_parents_key PRIMARY KEY (idMerge, idCommit)
 );
 
 CREATE TABLE Merge_Conflict(
-	idMerge INT , FOREIGN KEY(idMerge) REFERENCES Merge(id),
-	idConflict INT , FOREIGN KEY (idConflict) REFERENCES Conflict(id),
+	idMerge INT PRIMARY KEY, FOREIGN KEY(idMerge) REFERENCES Merge(id),
+	idConflict INT PRIMARY KEY, FOREIGN KEY (idConflict) REFERENCES Conflict(id),
 	CONSTRAINT merge_conflict_key PRIMARY KEY (idMerge, idConflict)
 );
 
 CREATE TABLE Conflict_ConflictRegion(
-	idConflict INT , FOREIGN KEY (idConflict) REFERENCES Conflict(id),
-	idConflictRegion INT , FOREIGN KEY (idConflictRegion) REFERENCES ConflictRegion(id),
+	idConflict INT PRIMARY KEY, FOREIGN KEY (idConflict) REFERENCES Conflict(id),
+	idConflictRegion INT PRIMARY KEY, FOREIGN KEY (idConflictRegion) REFERENCES ConflictRegion(id),
 	CONSTRAINT conflict_conflictRegion_key PRIMARY KEY (idConflict, idConflictRegion)
 );
