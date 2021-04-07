9852fd5a4d05909be2ac9ef26e37d48c50295a7e
goldner <goldner@Jarvis-II>
2021-04-07 17:11:11 -0300

    upadate parameters type

--- Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/visitor/cpp/AST.java
+++ Core/src/main/java/br/ufjf/dcc/gmr/core/chunks/antlr4/visitor/cpp/AST.java
@@ -46,12 +46,12 @@ public class AST {
             if (parser.getNumberOfSyntaxErrors() > 0) {
                 // visitor = new Visitor1();
                 visitor2 = new Visitor2();
-                visitor3 = new Visitor3();
+                visitor3 = new Visitor3(visitor2.getMethodDeclaration());
                 comments = ANTLR4Tools.getCommentsFromChannel2(tokens, true, Language.CPP);
             } else {
                 //  visitor = new Visitor1();
                 visitor2 = new Visitor2();
-                visitor3 = new Visitor3();
+                visitor3 = new Visitor3(visitor2.getMethodDeclaration());
                 comments = ANTLR4Tools.getCommentsFromChannel2(tokens, true, Language.CPP);
             }
             // visitor.visit(tree);
@@ -113,15 +113,17 @@ public class AST {
             for (int j = 0; j < methodDeclaration.get(i).size(); j++) {
                 for (int k = 0; k < methodCall.size(); k++) {
                     for (int l = 0; l < methodCall.get(k).size(); l++) {
-                        //compara nome
-                        if (verifica(methodDeclaration.get(i).get(j).getName(), methodCall.get(k).get(l).getName())) {
-                            //compara quantidade de parametro
-                            if (methodDeclaration.get(i).get(j).getQtParameters() == methodCall.get(k).get(l).getQtParameters()) {
-                                System.out.println("=============== COMPARA ==================");
-                                System.out.println("MD: " + methodDeclaration.get(i).get(j).toString() + " linha: "
-                                        + methodDeclaration.get(i).get(j).getCtx().getStart().getLine()
-                                        + "\nMC: " + methodCall.get(k).get(l).toString() + " linha: "
-                                        + methodCall.get(k).get(l).getCtx().getStart().getLine());
+                        if(i != k) {
+                                //compara nome
+                            if (verifica(methodDeclaration.get(i).get(j).getName(), methodCall.get(k).get(l).getName())) {
+                                //compara quantidade de parametro
+                                if (methodDeclaration.get(i).get(j).getQtParameters() == methodCall.get(k).get(l).getQtParameters()) {
+                                    System.out.println("=============== COMPARA ==================");
+                                    System.out.println("MD: " + methodDeclaration.get(i).get(j).toString() + " linha: "
+                                            + methodDeclaration.get(i).get(j).getCtx().getStart().getLine()
+                                            + "\nMC: " + methodCall.get(k).get(l).toString() + " linha: "
+                                            + methodCall.get(k).get(l).getCtx().getStart().getLine());
+                                }
                             }
                         }
                     }
@@ -136,8 +138,8 @@ public class AST {
 
     public static void main(String args[]) throws IOException {
 
-        String path = "/home/ketleen/Documentos/grafos-master/main.cpp";
-        String pathh = "/home/ketleen/Documentos/grafos-master/header/Grafo.h";
+        String path = "/home/goldner/Documentos/grafos-master/main.cpp";
+        String pathh = "/home/goldner/Documentos/grafos-master/header/Grafo.h";
         analyzeCPPSyntaxTree(path, true);
         analyzeCPPSyntaxTree(pathh, true);
         comparaNome();
